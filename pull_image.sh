f0d40633230cd780b7c1df13414db2dad48a2cae
xyz349925756 <xyz349925756@gmail.com>
2024-04-17 21:56:01 +0800

    v1.29.4

--- pull_image.sh
+++ pull_image.sh
@@ -1,16 +1,16 @@
 #/bin/bash
 One_Image=(
-  kube-apiserver:v1.23.5 
-  kube-controller-manager:v1.23.5 
-  kube-scheduler:v1.23.5 
-  kube-proxy:v1.23.5 
-  etcd:3.5.1-0
-  pause:3.6
+  kube-apiserver:v1.29.4 
+  kube-controller-manager:v1.29.4
+  kube-scheduler:v1.29.4
+  kube-proxy:v1.29.4
+  etcd:3.5.12-0
+  pause:3.9
 )
 
 Two_Image=(
-  metrics-server:v0.5.2
-  coredns:v1.8.6
+  metrics-server:v0.7.1
+  coredns:v1.11.1
 )
 
 Three_Image=(
@@ -21,20 +21,20 @@ Three_Image=(
 for imagename in ${One_Image[@]};
 do 
   docker pull docker.io/xyz349925756/${imagename}
-  docker tag docker.io/xyz349925756/${imagename} k8s.gcr.io/${imagename}
+  docker tag docker.io/xyz349925756/${imagename} registry.k8s.io/${imagename}
   docker rmi docker.io/xyz349925756/${imagename}
 done
 
 for imagename2 in ${Two_Image[@]};
 do 
   docker pull docker.io/xyz349925756/${imagename2}
-  docker tag docker.io/xyz349925756/${imagename2} k8s.gcr.io/`echo $imagename2|awk -F":" '{print $1}'`/${imagename2}
+  docker tag docker.io/xyz349925756/${imagename2} registry.k8s.io/`echo $imagename2|awk -F":" '{print $1}'`/${imagename2}
   docker rmi docker.io/xyz349925756/${imagename2}
 done
 
 for imagename3 in ${Three_Image[@]};
 do 
   docker pull docker.io/xyz349925756/${imagename3}
-  docker tag docker.io/xyz349925756/${imagename3} k8s.gcr.io/ingress-nginx/`echo $imagename3|awk -F"nginx-" '{print $2}'`
+  docker tag docker.io/xyz349925756/${imagename3} registry.k8s.io/ingress-nginx/`echo $imagename3|awk -F"nginx-" '{print $2}'`
   docker rmi docker.io/xyz349925756/${imagename3}
 done
